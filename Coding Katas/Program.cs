05db1eaec0be5be0bf3ed20c8eb8bd8081af73f1
Matt <mathias.luer@gmail.com>
2018-06-30 17:07:32 +0200

    Start Project

---
+++ Coding Katas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Coding_Katas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int x = 1, y = 2;
+
+            double result = Convert.ToDouble(x/y);
+            Console.WriteLine(result);
+
+            result = Convert.ToDouble(x) / Convert.ToDouble(y);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
